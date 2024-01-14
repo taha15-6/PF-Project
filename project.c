5325ac9de816dfc337b57c5946f7d6970df1e18f
taha15-6 <117084455+taha15-6@users.noreply.github.com>
2024-01-14 17:53:19 +0500

    A king's Duty
    Final code of the game

--- project.c
+++ project.c
@@ -7,17 +7,16 @@
 #define BOARD_SIZE 10
 #define MAX_HEALTH 10
 
-// ANSI escape codes for text color
-#define BLUE_TEXT "\033[34m"  // Blue
-#define LIGHT_BLUE_TEXT "\033[94m"  // Light Blue
-#define ORANGE_TEXT "\033[33m"  // Yellow-Orange
-#define GREEN_TEXT "\033[32m"  // Green
-#define PURPLE_TEXT "\033[35m"  // Purple
-#define RED_TEXT "\033[31m"     // Red
-#define BROWN_TEXT "\033[0;33m" // Brown
-#define DARK_BROWN_TEXT "\033[0;33;40m"  // Dark Brown
-#define LIGHT_PINK_TEXT "\033[95m" // Light Pink
-#define RESET_TEXT "\033[0m"   // Reset to default color
+#define BLUE_TEXT "\033[34m"  
+#define LIGHT_BLUE_TEXT "\033[94m"  
+#define ORANGE_TEXT "\033[33m"  
+#define GREEN_TEXT "\033[32m"  
+#define PURPLE_TEXT "\033[35m"  
+#define RED_TEXT "\033[31m"  
+#define BROWN_TEXT "\033[0;33m" 
+#define DARK_BROWN_TEXT "\033[0;33;40m" 
+#define LIGHT_PINK_TEXT "\033[95m" 
+#define RESET_TEXT "\033[0m"  
 
 
 void displayBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
@@ -35,9 +34,9 @@ void displayBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
                     printf("\t%s%c%s ", ORANGE_TEXT, cell, RESET_TEXT);
                     break;
                 case 'O':
-		    printf("\t%s%c%s ", GREEN_TEXT, cell, RESET_TEXT);
+				    printf("\t%s%c%s ", GREEN_TEXT, cell, RESET_TEXT);
                     break;
-		case 'W':
+				case 'W':
                     printf("\t%s%c%s ", PURPLE_TEXT, cell, RESET_TEXT);
                     break;
                 case 'X':
@@ -46,6 +45,7 @@ void displayBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
                 case 'M':
                     printf("\t%s%c%s ", DARK_BROWN_TEXT, cell, RESET_TEXT);
                     break;    
+     
                 default:
                     printf("\t%c ", cell); 
             }
@@ -53,6 +53,7 @@ void displayBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
         printf("\n\n");
     }
 }
+
 void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     if (level == 1) {
@@ -69,8 +70,7 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
             {'L', 'L', 'L', '_', 'L', 'L', '_', 'L', 'L', 'L'},
             {'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L'}
         };
-	    
-        // Copy level1 to the main board
+
         SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
         printf("Pass Through The Lava Cave! And Find The Portal\n");
         printf(" Don't be Scared to take Damage. You are going to get Wounded in your Journey.\n");
@@ -84,7 +84,6 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
             }
         }
     } else if (level == 2) {
-        // Level 2 Board Configuration
         char level2[BOARD_SIZE][BOARD_SIZE] = {
             {'X', '_', '_', '_', '_', '_', '_', '_', '_', '_'},
             {'_', 'X', '_', 'W', 'X', 'W', '_', '_', '_', '_'},
@@ -98,25 +97,24 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
             {'K', '_', '_', 'X', '_', 'X', 'X', 'X', '_', 'W'}
         };
 
-        // Copy level2 to the main board
         SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
         printf("Fight The Creepy Creatures And Pass Through The Portal To Procede\n");
         printf("Don't Take Too Much DAMAGE!");
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        Beep(600,100);
+         Beep(600,100);
         Beep(2000,100);
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
                 board[i][j] = level2[i][j];
             }
         }
-     } else if (level == 3) {
+    } else if (level == 3) {
         // Level 3 Board Configuration
         char level3[BOARD_SIZE][BOARD_SIZE] = {
-       	    {'M', 'X', 'X', 'X', 'W', 'L', 'L', 'L', 'L', 'L'},
-            {'M', 'L', 'L', 'L', 'W', '_', 'L', 'L', 'L', 'L'},
-            {'M', 'L', 'Q', 'L', 'W', '_', 'X', 'L', 'L', 'L'},
-            {'M', 'L', 'L', 'L', 'W', '_', '_', '_', 'L', 'L'},
+       		{'M', 'X', 'X', 'X', 'M', 'L', 'L', 'L', 'L', 'L'},
+            {'W', 'L', 'L', 'L', 'W', '_', 'L', 'L', 'L', 'L'},
+            {'M', 'L', 'Q', 'L', 'M', '_', 'X', 'L', 'L', 'L'},
+            {'W', 'L', 'L', 'L', 'W', '_', '_', '_', 'L', 'L'},
             {'M', 'X', 'X', 'X', 'W', '_', '_', '_', '_', 'L'},
             {'L', 'X', '_', '_', '_', 'X', 'X', 'W', 'M', 'L'},
             {'L', 'L', '_', '_', '_', 'W', 'M', '_', 'X', 'L'},
@@ -124,7 +122,7 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
             {'L', 'L', 'L', '_', '_', 'X', '_', 'L', 'K', 'L'},
             {'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L'}
         };
-	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
         printf("\tPass Through All The Obstacles and Fight All The Enemies to Save Your Queen\n");
         printf("\t\t\t Don't DIE!!'");
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
@@ -140,45 +138,40 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE], int level) {
 int main() {
 	char playagain;
 	do{
-	system("cls");
-        int currentLevel = 1;
-        int winCount = 0;
-        int kingHealth = MAX_HEALTH;
+		system("cls");
+    int currentLevel = 1;
+    int winCount = 0;
+     int kingHealth = MAX_HEALTH;
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY );
         printf("\t\t\t\t\t           A KING's DUTY\n");
+        sleep(1);
         printf("\t\t\t\t\t    --Save Your Queen, KING!--\n");
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        int sleeptimer = 2;
-        sleep(sleeptimer);
-	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        sleep(2);
+		SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
         printf("\t\t\t\tOnce upon a time, in the magical kingdom of Eldoria,\n");
-        sleeptimer = 3;
-        sleep(sleeptimer);
+        sleep(3);
         printf("\t\t\t   a nefarious  sorcerer  kidnapped  your  beloved  Queen  and-\n");
-        sleep(sleeptimer);
+        sleep(3);
         printf("\t\t\tlocked her away in a mysterious dungeon guarded by deadly obstacles.\n");
-        sleeptimer = 4;
-        sleep(sleeptimer);
+      	sleep(3);
         printf("\t\t     Your  quest, noble King, is  to  navigate through the treacherous  lands,\n");
-        sleeptimer = 3;
-        sleep(sleeptimer);
+     	sleep(3);
         printf("\t\t   overcome  obstacles,  and  rescue  your  Queen  from  the  clutches  of  evil.\n");
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        sleeptimer = 4;
-        sleep(sleeptimer);
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY );
+    	sleep(3);
+          SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY );
         printf("\t\t\t\t\t Get  Ready  To  Enter The  Dungeon\n");
-        
-        sleeptimer = 3;
-        sleep(sleeptimer);
+        sleep(3);
+
         for (int i = 3; i >= 1 ; i--) {
             printf("\t\t\t\t\t\t\t %d\n\n", i);
-            sleeptimer = 2;
-            sleep(sleeptimer);
+             Beep(200,200);
+             sleep(1);
         }
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        while (currentLevel <= 3 && kingHealth > 0) {
+    while (currentLevel <= 3 && kingHealth > 0) {
         char board[BOARD_SIZE][BOARD_SIZE];
         printf("\n\n\n\n\n");
         initializeBoard(board, currentLevel);
@@ -204,7 +197,7 @@ int main() {
         while (!win && !gameover) {
             char move;
             printf("Enter Action for King (w/a/s/d): ");
-            move = getch(); // Use _getch() to capture key press without the need for Enter
+            move = getch(); 
 
             int prevKingRow = kingRow;
             int prevKingCol = kingCol;
@@ -212,22 +205,22 @@ int main() {
               switch (move) {
                 case 'd':
                     kingCol = (kingCol < BOARD_SIZE - 1) ? kingCol + 1 : kingCol;
-                Beep(400,100);  // Beep with a frequency of 1000 Hz for 500 milliseconds
+                Beep(400,100);  
                 Beep(1000,50);
 	                    break;
                 case 'a':
                     kingCol = (kingCol > 0) ? kingCol - 1 : kingCol;
-                    Beep(400,100);  // Beep with a frequency of 1000 Hz for 500 milliseconds
+                    Beep(400,100);  
                     Beep(1000,50);
                     break;
                 case 'w':
                     kingRow = (kingRow > 0) ? kingRow - 1 : kingRow;
-                    Beep(400,100);  // Beep with a frequency of 1000 Hz for 500 milliseconds
+                    Beep(400,100);  
                     Beep(1000,50);
                     break;
                 case 's':
                     kingRow = (kingRow < BOARD_SIZE - 1) ? kingRow + 1 : kingRow;
-                    Beep(400,100);  // Beep with a frequency of 1000 Hz for 500 milliseconds
+                    Beep(400,100);
                     Beep(1000,50);
                     break;
                 default:
@@ -270,7 +263,7 @@ int main() {
 
         // Move to the next level
         currentLevel++;
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+		system("cls");
     }
 
     if (winCount == 3) {
